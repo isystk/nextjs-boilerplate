2183dda2df69261295bec2af16ae9e850396898b
iseyoshitaka <ise0615@gmail.com>
2025-06-08 09:10:36 +0900

    SideMenuを追加

--- src/components/molecules/SideMenu/index.test.tsx
+++ src/components/molecules/SideMenu/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { describe, it, beforeEach, expect, vi } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/react';
 import * as stories from './index.stories';
 
